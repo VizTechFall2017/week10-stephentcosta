f7eb664e90b0a02136aaf2ff27ade5e87b07e78a
stephentcosta <31608698+stephentcosta@users.noreply.github.com>
2017-11-20 21:10:10 -0500

    Corrected folder name

--- PS01/index.js
+++ PS01/index.js
@@ -1,15 +1,29 @@
+// __      __     _____  _____          ____  _      ______  _____
+// \ \    / /\   |  __ \|_   _|   /\   |  _ \| |    |  ____|/ ____|
+//  \ \  / /  \  | |__) | | |    /  \  | |_) | |    | |__  | (___
+//   \ \/ / /\ \ |  _  /  | |   / /\ \ |  _ <| |    |  __|  \___ \
+//    \  / ____ \| | \ \ _| |_ / ____ \| |_) | |____| |____ ____) |
+//     \/_/    \_\_|  \_\_____/_/    \_\____/|______|______|_____/
+
+
+
+
 var width = document.getElementById('svg1').clientWidth;
 var height = document.getElementById('svg1').clientHeight;
 
 var marginLeft = 0;
 var marginTop = 0;
 //
-var svg = d3.select('svg')
+var svg = d3.select('#svg1')
+    .append('g')
+    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
+
+var svg2 = d3.select('#svg2')
     .append('g')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
 var projection = d3.geoAlbers()
-    .scale( 8000 )
+    .scale( 130000 )
     .rotate( [71.057,0] )
     .center( [0, 42.313] )
     .translate( [width/2,height/2] );
@@ -19,7 +33,7 @@ var projection = d3.geoAlbers()
 
 var areaLookup = d3.map();
 console.log(areaLookup);
-var colorScale = d3.scaleLinear().range(['white','#00a6b4']);
+var colorScale = d3.scaleLinear().range(['#F2F2F2','#FB4D42']);
 
 var div = d3.select("body")
 		    .append("div")
@@ -38,6 +52,18 @@ var div = d3.select("body")
 //     .attr("class", "area")
 //     .attr("stroke", "white")
 //     .attr("fill", "#00a6b4")
+
+
+
+
+
+
+//  _____ _    _  ____  _____   ____  _____  _      ______ _______ _    _
+// / ____| |  | |/ __ \|  __ \ / __ \|  __ \| |    |  ____|__   __| |  | |
+// | |   | |__| | |  | | |__) | |  | | |__) | |    | |__     | |  | |__| |
+// | |   |  __  | |  | |  _  /| |  | |  ___/| |    |  __|    | |  |  __  |
+// | |___| |  | | |__| | | \ \| |__| | |    | |____| |____   | |  | |  | |
+// \_____|_|  |_|\____/|_|  \_\\____/|_|    |______|______|  |_|  |_|  |_|
 
 queue()
     .defer(d3.json, "./bostonNeighborhoods.json")
@@ -62,16 +88,16 @@ queue()
             .attr('fill', function(d){
                 return colorScale(areaLookup.get(d.properties.Name));
             })
-            .attr('stroke','#003c50')
+            .attr('stroke','#FB4D42')
             .attr('stroke-width',2)
               .on("mouseover", function(d) {
 console.log(areaLookup.get(d.properties.Name));
                   	div.transition()
                     	   .duration(200)
-                         .style("opacity", .9);
-                         div.text(d.properties.Name + areaLookup.get(d.properties.Name))
-                         .style("left", (d3.event.pageX) + "px")
-                         .style("top", (d3.event.pageY - 28) + "px");
+                         .style("opacity", 1);
+                         div.html(d.properties.Name  +  "<br/>" + areaLookup.get(d.properties.Name))
+                         .style("left", (d3.event.pageX - 80) + "px")
+                         .style("top", (d3.event.pageY - 80) + "px");
           	})
 
               // fade out tooltip on mouse out
@@ -82,6 +108,85 @@ console.log(areaLookup.get(d.properties.Name));
               });
                 });
 
+
+
+ //  _____ _____ ______    _____ _    _          _____ _______
+ // |  __ \_   _|  ____|  / ____| |  | |   /\   |  __ \__   __|
+ // | |__) || | | |__    | |    | |__| |  /  \  | |__) | | |
+ // |  ___/ | | |  __|   | |    |  __  | / /\ \ |  _  /  | |
+ // | |    _| |_| |____  | |____| |  | |/ ____ \| | \ \  | |
+ // |_|   |_____|______|  \_____|_|  |_/_/    \_\_|  \_\ |_|
+
+
+                var pieX = width/2 - marginLeft;
+                var pieY = height/2 - marginTop;
+
+                var pieGroup = svg2.append('g')
+                    .attr('transform', 'translate(' + pieX + ',' + pieY + ')');
+
+                //set up scales to position circles using the data
+                var scaleColor = d3.scaleOrdinal()
+                    .domain(["16-19", "20-24", "25-34", "35-44", "45-54", "55-64","65+"])
+                    .range(["#BB3931","#7B2620","#FB4D42","#3C1210","#E1453B","#E1593B","#E17F6C"]);
+                //var scaleY = d3.scaleLinear().domain([0,1200]).range([400, 0]);  //remember that 0,0 is at the top of the screen! 300 is the lowest value on the y axis
+
+                var nestedData = [];
+
+                var pieRadius = 100;
+
+                var makeArc = d3.arc()
+                    .outerRadius(pieRadius)
+                    .innerRadius(75);
+
+                var labelArc = d3.arc()
+                    .outerRadius(pieRadius + 30)
+                    .innerRadius(pieRadius + 30);
+
+                var makePie = d3.pie()
+                    .sort(null)
+                    .value(function(d) { return d.total; });
+
+                //import the data from the .csv file
+                d3.csv('./ageDist.csv', function(dataIn){
+
+                    nestedData = d3.nest()
+                        .key(function(d){return d.year})
+                        .entries(dataIn);
+
+                    var loadData = dataIn;
+
+                    // svg2.append('text')
+                    //     .text('Age Distribution')
+                    //     .attr('transform','translate(300, 20)')
+                    //     .attr('text-anchor','middle')
+                    //     .attr('fill', 'white');
+
+
+
+                    // make a group for each arc, which will contain the path for the arc
+                    var g = pieGroup.selectAll('.arc')
+                        .data(makePie(loadData))   //makePie makes sure that one arc gets added for each data object in the array
+                        .enter()
+                        .append('g')
+                        .attr('class','arc');
+
+                    g.append('path')              //grab each group in the variable above, and add a path to it (this will be the pie wedge)
+                        .attr('d',makeArc)        //call the makeArc generator function to draw the actual wedges
+                        .attr('fill', function(d){ return scaleColor(d.data.age)});   //give the wedges a color, based on their d.age values
+
+
+                    g.append("text")
+                        .attr("transform", function(d) {return "translate(" + labelArc.centroid(d) + ")"; })
+                        .attr("dy", ".35em")
+                        .attr('text-anchor','middle')
+                        .attr('fill', '#FB4D42')
+                        .attr('font-family', 'Helvetica')
+                        .attr('font-weight', 'Bold')
+                        .text(function(d) { return d.data.age; });
+                });
+
+                d3.selectAll("text")
+                      .attr('fill', 'white');
 
 
 
